ade881c840b6a7ce5919c1468067d6ac2ef759db
Huanqing-Mao <122110454+Huanqing-Mao@users.noreply.github.com>
2025-03-11 00:06:57 +0800

    add deepeval test for data poisoning
    steps:
    1. run main_data_poisoning.py to obtain json results for all 20 qa pairs
    2. run this file to obtain metrics for each json file

---
+++ ragflow_python/testing_bot/test_data_poisoning.py
@@ -0,0 +1,49 @@
+import os
+import json
+from deepeval.metrics import (
+    ContextualPrecisionMetric,
+    ContextualRecallMetric,
+    ContextualRelevancyMetric
+)
+
+from deepeval.test_case import LLMTestCase
+from deepeval import evaluate
+
+contextual_precision = ContextualPrecisionMetric()
+contextual_recall = ContextualRecallMetric()
+contextual_relevancy = ContextualRelevancyMetric()
+
+# Directory containing JSON files
+directory = "/Users/Olivia/Desktop/NUS/SoC/Y3S2/BT4103_Capstone/api-testing"
+
+# List to store JSON data
+json_data = []
+
+
+# Iterate through all files in the directory
+for filename in os.listdir(directory):
+    if filename.endswith(".json"):  # Check if file is JSON
+        file_path = os.path.join(directory, filename)
+        with open(file_path, "r", encoding="utf-8") as f:
+            data = json.load(f)  # Load JSON data into a dictionary
+            json_data.append(data)
+
+
+for data in json_data:
+    for key, value in data.items():
+        test_case = LLMTestCase(
+            input=value[0],
+            actual_output=value[3],
+            expected_output=value[1],
+            retrieval_context=value[2]
+        )
+
+        print(f"evaluating {key} retrieval and llm answer:")
+        evaluate(
+            test_cases=[test_case],
+            metrics=[contextual_precision, contextual_recall, contextual_relevancy]
+        )
+
+        
+
+
