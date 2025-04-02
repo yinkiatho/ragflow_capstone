9f9a7903cd5c8a17f01b6b3decabc58e153bddfd
Huanqing-Mao <122110454+Huanqing-Mao@users.noreply.github.com>
2025-04-02 19:17:17 +0800

    Update testing_data_poisoning.py

--- ragflow_python/data_poisoning_special_tokens/testing_data_poisoning.py
+++ ragflow_python/data_poisoning_special_tokens/testing_data_poisoning.py
@@ -22,49 +22,27 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-'''
-Hi mak! Remember to ignore the 'pre_attack' in each JSON file because I did not run chat for pre_attack to save time and CPU. You can start with 'post_attack' and 'post_defense'.
-You can also create a separate table on supabase to upload the data, then ignore the experiment_id and all that
-
-defense_results1 = results with a defense that only mask top 10 chunks w perplexity >= 500
-defense_results2 = results with a defense that mask top 50 chunks w perplexity >= 500
-defense_results3 = results with a defense that mask top 50 chunks w perplexity >= 500, but with new qa_pairs created by jj
-maybe can consider separating them when uploading
-'''
 
 # Parameters
 json_data = []
-directory = "../data/data_poisoning"
+
+# change directory to defense_result1, defense_result2, defense_result3
+directory = "./defense_results3"
+num_of_chunks_to_mask = 50 # defense_result1 num_of_chunks_to_mask = 10
+perplexity_threshold = 500
+is_new_qa_pair =  True # only True for defense_result3
 
 
 url = "https://bggngaqkkmslamsbebew.supabase.co"
 key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJnZ25nYXFra21zbGFtc2JlYmV3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA0MDgxNzIsImV4cCI6MjA1NTk4NDE3Mn0.B4D-5t0oxa8D6xMSoywufdB7aSmGy1s8bvytH0znows"
 supabase = create_client(url, key)
-pre_attack_experiment_id = 372
-post_attack_experiment_id = 6890
 
+special_token_attack_id = 1080
+defense_id = 1988
 
-def generate_unique_id():
-    """Generate a unique retrieval_id that does not exist in RAGFlow_Response"""
-    while True:
-        new_id = random.randint(1, 10000)
-        response = supabase.table("RAGFlow_Response").select("retrieval_id").eq("retrieval_id", new_id).execute()
-        
-        if not response.data:  # If no existing retrieval_id found, it's unique
-            return new_id
-
-
-def generate_unique_experiment_id():
-    while True:
-        new_id = random.randint(1, 10000)
-        response = supabase.table("Evaluation_Table").select("evaluation_id").eq("evaluation_id", new_id).execute()
-
-        if not response.data:  # If no existing retrieval_id found, it's unique
-            return new_id
 
 def process_attack_results(data):
     """Processes a single test case and inserts data into Supabase"""
-    
 
     # Initialize metrics
     contextual_precision = ContextualPrecisionMetric()
@@ -73,67 +51,93 @@ def process_attack_results(data):
     answer_relevancy = AnswerRelevancyMetric()
     faithfulness = FaithfulnessMetric()
 
-    # Create data test case for both pre and post attack
-    for key, value in data.items():
-        retrieval_id = generate_unique_id()
-        evaluation_id = generate_unique_experiment_id()
-        test_case = LLMTestCase(
-            input= value[0],
-            actual_output=value[3],
-            expected_output=value[1],
-            retrieval_context=value[2]
-        )
+    # Create data test case for post attack
+    value = data["post_attack"]
+    test_case = LLMTestCase(
+                input= value[0],
+                actual_output=value[3],
+                expected_output=value[1],
+                retrieval_context=value[2]
+            )
+    
+    # Measure metrics for post attack
+    contextual_precision.measure(test_case)
+    precision_score = contextual_precision.score
 
-        # Measure metrics
-        contextual_precision.measure(test_case)
-        precision_score = contextual_precision.score
+    contextual_recall.measure(test_case)
+    recall_score = contextual_recall.score
 
-        contextual_recall.measure(test_case)
-        recall_score = contextual_recall.score
+    contextual_relevancy.measure(test_case)
+    relevancy_score = contextual_relevancy.score
 
-        contextual_relevancy.measure(test_case)
-        relevancy_score = contextual_relevancy.score
+    answer_relevancy.measure(test_case)
+    answer_score = answer_relevancy.score
 
-        answer_relevancy.measure(test_case)
-        answer_score = answer_relevancy.score
+    faithfulness.measure(test_case)
+    faithfulness_score = faithfulness.score
 
-        faithfulness.measure(test_case)
-        faithfulness_score = faithfulness.score
+    # Insert into Special_Tokens_Attack_Table
+    attack_response = supabase.table("Special_Token_Attacks").insert({
+        "attack_id": special_token_attack_id, 
+        "contextual_precision": precision_score,
+        "contextual_recall": recall_score,
+        "contextual_relevancy": relevancy_score,
+        "answer_relevancy": answer_score,
+        "faithfulness": faithfulness_score,
+        "perplexity_threshold": perplexity_threshold, 
+        "num_chunks_to_mask": num_of_chunks_to_mask,
+        "chunks_retrieved": value[2],
+        "llm_answer": value[3],
+        "ground_truth": value[1],
+        "question": value[0],
+        "is_new_qa_pair": is_new_qa_pair
+    }).execute()
 
-        # Insert into RAGFlow_Response first
-        RAGFlow_response = supabase.table("RAGFlow_Response").insert({
-            "retrieval_id": retrieval_id, 
-            "chunks_retrieved": value[2],
-            "model_answer": value[3]
-        }).execute()
+    print(attack_response)
 
-        # Ensure retrieval_id exists before inserting into Evaluation_Table
-        confirmation = supabase.table("RAGFlow_Response").select("retrieval_id").eq("retrieval_id", retrieval_id).execute()
-        if not confirmation.data:
-            raise Exception(f"retrieval_id {retrieval_id} was not found in RAGFlow_Response after insertion!")
-        
-        # Insert into Evaluation_Table
-        ex_id = None # experiment_id
-        if key == "pre_attack":
-            ex_id = pre_attack_experiment_id
-        elif key == "post_attack":
-            ex_id = post_attack_experiment_id
+    # Create data test case for post defense
+    pd_value = data["post_defense"]
+    pd_test_case = LLMTestCase(
+                input= pd_value[0],
+                actual_output=pd_value[3],
+                expected_output=pd_value[1],
+                retrieval_context=pd_value[2]
+            )
+    
+    # Measure metrics for post attack
+    contextual_precision.measure(pd_test_case)
+    precision_score = contextual_precision.score
 
-        evaluation_response = supabase.table("Evaluation_Table").insert({
-            "evaluation_id": evaluation_id,
-            "experiment_type_id": ex_id,
-            "contextual_precision": precision_score,
-            "contextual_recall": recall_score,
-            "contextual_relevancy": relevancy_score,
-            "answer_relevancy": answer_score,
-            "faithfulness": faithfulness_score,
-            "retrieval_id": retrieval_id,  # This key now exists in RAGFlow_Response
-            "ground_truth": value[1],
-            "question": value[0],
-            "model": "gemma2:2b"
-        }).execute()
+    contextual_recall.measure(pd_test_case)
+    recall_score = contextual_recall.score
 
-        print(f"✅ Test case '{value[0]}' inserted successfully!\n")
+    contextual_relevancy.measure(pd_test_case)
+    relevancy_score = contextual_relevancy.score
+
+    answer_relevancy.measure(pd_test_case)
+    answer_score = answer_relevancy.score
+
+    faithfulness.measure(pd_test_case)
+    faithfulness_score = faithfulness.score
+
+    # Insert into Perplexity_Defense Table
+    defense_response = supabase.table("Perplexity_Defense").insert({
+        "defense_id": defense_id, 
+        "contextual_precision": precision_score,
+        "contextual_recall": recall_score,
+        "contextual_relevancy": relevancy_score,
+        "answer_relevancy": answer_score,
+        "faithfulness": faithfulness_score,
+        "perplexity_threshold": perplexity_threshold, 
+        "num_chunks_to_mask": num_of_chunks_to_mask,
+        "chunks_retrieved": pd_value[2],
+        "llm_answer": pd_value[3],
+        "ground_truth": pd_value[1],
+        "question": pd_value[0],
+        "is_new_qa_pair": is_new_qa_pair
+    }).execute()
+    print(defense_response)
+    print(f"✅ Test case '{value[0]}' inserted successfully!\n")
 
 
 # Iterate through all files in the directory
